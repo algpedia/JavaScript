489b3081065961e8b1b33077c87a8d00b2cfc0e8
jppianta <joao.pianta@outlook.com>
2020-06-20 01:11:18 -0300

    adiciona comentarios

--- Busca/binaria.js
+++ Busca/binaria.js
@@ -3,11 +3,16 @@ function buscaBinaria(lista, alvo) {
 }
 
 function buscaBinariaRecursiva(lista, inicio, fim, alvo) {
+  // Se os ponteiros de inicio e fim colidirem, significa que estamos buscando em um intervalo vazio. 
+  // Logo o alvo não está na lista
   if (inicio >= fim) {
     return -1;
   }
+
+  // Salva o ponto do meio da lista
   const meio = Math.floor((inicio + fim) / 2);
 
+  // Define os limites da busca dependendo se o alvo for igual, menor ou maior que o ponto do meio atual
   if (lista[meio] === alvo) {
     return meio;
   } else if (lista[meio] > alvo) {
--- Busca/linear.js
+++ Busca/linear.js
@@ -1,9 +1,13 @@
 function buscaLinear(lista, alvo) {
-  for(let i=0; i<lista.length; i++) {
+  // Verifica todos os elementos
+  for (let i = 0; i < lista.length; i++) {
+    // Se o valor da posição for igual ao alvo, retorna o indíce
     if (lista[i] === alvo) {
       return i;
     }
   }
+
+  // Caso valor não tenha sido encontrado, retorna -1
   return -1;
 }
 
